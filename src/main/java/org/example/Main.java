bb078daec1afa1605cd775e141ebcb2994c11e83
Daniil <d.dubina@mobidev.biz>
2023-10-08 17:54:40 +0300

    updateUserRequest deleteUserRequest getAllCustomersResponse

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,19 +13,47 @@ public class Main {
 
         User userRequest = User.builder()
                 .name("Daniil")
-                .phone("+3805023523545")
+                .phone("+380502352354")
                 .build();
 
-        String uri = "https://jsonplaceholder.typicode.com/users";
+        String uri = "https://jsonplaceholder.typicode.com";
         HttpRequest createUser = HttpRequest.newBuilder(new URI(uri))
                 .method("POST", HttpRequest.BodyPublishers.ofString(gson.toJson(userRequest)))
                 .version(HttpClient.Version.HTTP_1_1)
                 .build();
-
         HttpClient httpClient = HttpClient.newHttpClient();
         HttpResponse<String> response = httpClient.send(createUser, HttpResponse.BodyHandlers.ofString());
-
         System.out.println("response.statusCode() = " + response.statusCode());
         System.out.println("response.body() = " + response.body());
+        System.out.println();
+
+        User updateUserBody = User.builder()
+                .userName("I7Pi3PAk")
+                .build();
+        HttpRequest updateUserRequest = HttpRequest.newBuilder(new URI(uri + "/users" + 10))
+                .PUT(HttpRequest.BodyPublishers.ofString(gson.toJson(updateUserBody)))
+                .version(HttpClient.Version.HTTP_1_1)
+                .build();
+        HttpResponse<String> updateResponse = httpClient.send(updateUserRequest, HttpResponse.BodyHandlers.ofString());
+        System.out.println("updateResponse.statusCode() = " + updateResponse.statusCode());
+        System.out.println("updateResponse.body() = " + updateResponse.body());
+        System.out.println();
+
+        HttpRequest deleteUserRequest = HttpRequest.newBuilder(new URI(uri + "/users" + 10))
+                .DELETE()
+                .build();
+        HttpResponse<String> deleteResponse = httpClient.send(deleteUserRequest, HttpResponse.BodyHandlers.ofString());
+        System.out.println("deleteResponse.statusCode() = " + deleteResponse.statusCode());
+        System.out.println();
+
+        HttpRequest getAllCustomers = HttpRequest.newBuilder(new URI(uri+ "/users"))
+                .GET()
+                .build();
+        HttpResponse<String> getAllCustomersResponse = httpClient.send(getAllCustomers, HttpResponse.BodyHandlers.ofString());
+        System.out.println("getAllCustomersResponse.statusCode() = " + getAllCustomersResponse.statusCode());
+        System.out.println("getAllCustomersResponse.body() = " + getAllCustomersResponse.body());
+        System.out.println();
+
+
     }
 }
